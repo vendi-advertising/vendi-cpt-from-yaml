aa9b31add53021d8531d1bcefe848373e337b949
Chris Haas <chris@vendiadvertising.com>
2020-05-15 16:55:41 -0500

    Removed PHP Linter

--- run-phpunit.sh
+++ run-phpunit.sh
@@ -4,7 +4,6 @@
 # Use -gt 1 to consume two arguments per pass in the loop
 # Use -gt 0 to consume one or more arguments per pass in the loop
 
-RUN_LINT=true
 RUN_SEC=true
 RUN_PHP_CS=true
 RUN_PHP_UNIT=true
@@ -16,10 +15,6 @@ do
 
             --no-run-php-cs)
                 RUN_PHP_CS=false
-            ;;
-
-            --no-lint)
-                RUN_LINT=false
             ;;
 
             --no-php-unit)
@@ -45,27 +40,6 @@ maybe_run_php_cs()
         if [ $? -ne 0 ]; then
         {
             echo "Error with composer... exiting";
-            exit 1;
-        }
-        fi
-    }
-    else
-        echo "skipping";
-    fi
-
-    printf "\n";
-}
-
-maybe_run_linter()
-{
-    echo "Maybe running linter...";
-    if [ "$RUN_LINT" = true ]; then
-    {
-        echo "running...";
-        ./vendor/bin/parallel-lint --exclude vendor/ .
-        if [ $? -ne 0 ]; then
-        {
-            echo "Error with PHP linter... exiting";
             exit 1;
         }
         fi
@@ -118,7 +92,6 @@ if [ ! -d './vendor' ]; then
     composer update
 fi
 
-maybe_run_linter;
 maybe_run_php_cs;
 maybe_run_security_check;
 maybe_run_php_unit;
